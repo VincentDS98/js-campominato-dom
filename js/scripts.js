e0741f77c13d6fca62e04ce5576772fd984cd33b
VincentDS98 <disanto.vincenzo98@gmail.com>
2023-12-19 21:15:21 +0100

    Fine-Gioco

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ const playButton = document.getElementById('play-button');
 playButton.addEventListener('click', function(){
     const gridContainer = document.querySelector('.grid-container');
     const bombs = [];
+    
+
+
+
     while(bombs.length < 16){
         const newRandomNumber = getRandomNumber(1,100);
         console.log('newRandomNumber', newRandomNumber, typeof newRandomNumber);
@@ -18,22 +22,44 @@ playButton.addEventListener('click', function(){
     console.log('bombs' ,bombs, typeof bombs);
 
     gridContainer.innerHTML = '';
+    
 
     for(let i = 1 ; i <= 100; i++){
         const cell = document.createElement('div');
         cell.innerHTML ='<span>' + i + '</span>'  ;
         cell.classList.add('cell');
-        cell.addEventListener('click', function(){
-            const numberInCell = parseInt(this.innerText);
-            
-            
 
-            if(bombs.includes(numberInCell) == false) {
-                this.classList.add('click');
-            }
-            else {
-                this.classList.add('bomb');
-            }
+
+        cell.addEventListener('click', function(){
+            const allClickBombs = document.querySelectorAll('.cell.bomb');
+            console.log('Bombe Cliccate', allClickBombs.length);
+            const allClickedCell = document.querySelectorAll('.cell.click');
+                console.log('Punti', allClickedCell.length);
+            
+            if(
+                allClickBombs.length == 0 && allClickedCell.length < (100 -16)
+            ){
+
+                if(allClickBombs.length == 0) {
+                    const numberInCell = parseInt(this.innerText);
+                    
+
+                    if (bombs.includes(numberInCell)== false){
+                        this.classList.add('click');
+                        if(document.querySelector('.cell.click').length == (100 - 16)){
+                            alert('IL TUO PUNTEGGIO = ' + document.querySelectorAll(.cell.click).length);
+                        }
+                    }
+                    else {
+                    this.classList.add('bomb');
+
+                    alert('HAI PERSO! IL TUO PUNTEGGIO = ' + allClickedCell.length );
+                    }
+                }
+           
+
+             }
+
         });
         
     gridContainer.append(cell);
